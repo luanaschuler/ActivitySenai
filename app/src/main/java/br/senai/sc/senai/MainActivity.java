7bf237a5d7447c05a88b948726d9cd2f6a7ed544
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2020-10-05 22:20:37 -0300

    Initial commit!

---
+++ app/src/main/java/br/senai/sc/senai/MainActivity.java
@@ -0,0 +1,35 @@
+package br.senai.sc.senai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.editText = findViewById(R.id.et_texto);
+        this.editText2 = findViewById(R.id.et_texto2);
+    }
+
+    public void onClickBtnMostrar (View v) {
+        EditText editText = findViewById(R.id.et_texto);
+        Toast.makeText(MainActivity.this, editText.getText().toString() + " " + editText2.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickBtnLimpar (View v) {
+        editText.setText(" ");
+        editText2.setText(" ");
+    }
+
+
+}
